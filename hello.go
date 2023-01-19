c1247f382f7dc4b0cb16e517f54ffca465cdb3e7
Rlasserre <r_lasserre@hotmail.com>
2023-01-19 11:10:42 -0300

    update

--- hello.go
+++ hello.go
@@ -1,14 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	var name string = "Rafael"
+	var name = "Rafael"
 	var version float32 = 1.1
-	var age int = 38
+	var age = 38
 	fmt.Println("My first go program!")
 	fmt.Println("Hi, sr.", name, "!")
 	fmt.Println("You are", age, "y.o.")
 	fmt.Println("Program version:", version)
+
+	fmt.Println(`The type of the variable "name" is`, reflect.TypeOf(name))
 
 }
