4bbbc4ad130ff98170152f50089a4631d13e32d4
Rlasserre <r_lasserre@hotmail.com>
2023-01-19 11:35:22 -0300

    update

--- hello.go
+++ hello.go
@@ -13,8 +13,18 @@ func main() {
 	fmt.Println("1- Start Monitoring")
 	fmt.Println("2- Show Logs")
 	fmt.Println("0- Exit Program")
+
 	var comand int
 	fmt.Scan(&comand)
-
 	fmt.Println("You choose option", comand)
+
+	if comand == 1 {
+		fmt.Println("Start monitoring ...")
+	} else if comand == 2 {
+		fmt.Println("Showing Logs ...")
+	} else if comand == 0 {
+		fmt.Println("Leaving program.")
+	} else {
+		fmt.Println("Unavailable option.")
+	}
 }
