02480f6f589409ff66a261d61c17ed2c7923be23
Rlasserre <r_lasserre@hotmail.com>
2023-01-23 17:52:46 -0300

    update

--- hello.go
+++ hello.go
@@ -5,30 +5,12 @@ import (
 )
 
 func main() {
-	name := "Rafael"
-	var version float32 = 1.1
-	fmt.Println("Hi, sr.", name, "!")
-	fmt.Println("Program version:", version)
 
-	fmt.Println("1- Start Monitoring")
-	fmt.Println("2- Show Logs")
-	fmt.Println("0- Exit Program")
+	showIntro()
+	showMenu()
+	command := readCommand()
 
-	var comand int
-	fmt.Scan(&comand)
-	fmt.Println("You choose option", comand)
-
-	/* if comand == 1 { */
-	/* 	fmt.Println("Start monitoring ...") */
-	/* } else if comand == 2 { */
-	/* 	fmt.Println("Showing Logs ...") */
-	/* } else if comand == 0 { */
-	/* 	fmt.Println("Leaving program.") */
-	/* } else { */
-	/* 	fmt.Println("Unavailable option.") */
-	/* } */
-
-	switch comand {
+	switch command {
 	case 1:
 		fmt.Println("Start monitoring ...")
 	case 2:
@@ -38,5 +20,26 @@ func main() {
 	default:
 		fmt.Println("Unavailable option.")
 	}
+}
 
+func showIntro() {
+
+	name := "Rafael"
+	version := 1.1
+	fmt.Println("Hi, sr.", name, "!")
+	fmt.Println("Program version:", version)
+}
+
+func showMenu() {
+	fmt.Println("1- Start Monitoring")
+	fmt.Println("2- Show Logs")
+	fmt.Println("0- Exit Program")
+}
+
+func readCommand() int {
+	var commandRead int
+	fmt.Scan(&commandRead)
+	fmt.Println("You choose option", commandRead)
+
+	return commandRead
 }
